091d57e6937c2358e7c4104417805acfd588916f
Samina Khan <skhan191@gmail.com>
2016-12-28 21:49:24 -0500

    Day 2 part 2

--- day2/Day2.java
+++ day2/Day2.java
@@ -4,11 +4,24 @@ import java.util.*;
 public class Day2 {
   public static void main(String[] args) {
     ArrayList<String> data = readData("data.txt");
+    
+    // part 1
+    System.out.println("Part 1");
     int[] position = new int[]{0,0};
     for(String line : data) {
       position = getNextCode(position, line);
       System.out.printf("[" + position[0] + ", " + position[1] + "]");
       int code = readCode(position);
+      System.out.println(" --> " + code);
+    }
+    
+    // part 2
+    System.out.println("Part 2");
+    position = new int[]{-2, 0};
+    for(String line : data) {
+      position = getNextFancyCode(position, line);
+      System.out.printf("[" + position[0] + ", " + position[1] + "]");
+      String code = readFancyCode(position);
       System.out.println(" --> " + code);
     }
   }
@@ -47,7 +60,6 @@ public class Day2 {
 
   public static int[] getNextCode(int[] position, String line) {
     int[] nextPosition = position;
-
     for(int i = 0; i < line.length(); i++) {
       switch(line.charAt(i)) {
         case 'U':
@@ -73,5 +85,60 @@ public class Day2 {
     if(move < -1) return -1;
     if(move > 1) return 1;
     return move;
+  }
+  
+  // interpret fancy keypad as
+  //                    [0, 2]
+  //          [-1, 1],  [0, 1],  [1, 1]
+  // [-2, 0], [-1, 0],  [0, 0],  [1, 0], [2, 0]
+  //          [-1, -1], [0, -1], [1, -1]
+  //                    [0, -2]
+  
+  public static int[] getNextFancyCode(int[] position, String line) {
+    int[] nextPosition = position;
+    for(int i = 0; i < line.length(); i++) {
+      switch(line.charAt(i)) {
+        case 'U':
+          nextPosition = fancyMove(nextPosition, new int[]{0, 1});
+          break;
+        case 'D':
+        nextPosition = fancyMove(nextPosition, new int[]{0, -1});
+          break;
+        case 'R':
+        nextPosition = fancyMove(nextPosition, new int[]{1, 0});
+          break;
+        case 'L':
+        nextPosition = fancyMove(nextPosition, new int[]{-1, 0});
+          break;
+        default:
+          break;
+      }
+    }
+    return nextPosition;
+  }
+  
+  public static String readFancyCode(int[] position) {
+    if (position[0] == 0 && position[1] == 2) return "1";
+    if (position[0] == -1 && position[1] == 1) return "2";
+    if (position[0] == 0 && position[1] == 1) return "3";
+    if (position[0] == 1 && position[1] == 1) return "4";
+    if (position[0] == -2 && position[1] == 0) return "5";
+    if (position[0] == -1 && position[1] == 0) return "6";
+    if (position[0] == 0 && position[1] == 0) return "7";
+    if (position[0] == 1 && position[1] == 0) return "8";
+    if (position[0] == 2 && position[1] == 0) return "9";
+    if (position[0] == -1 && position[1] == -1) return "A";
+    if (position[0] == 0 && position[1] == -1) return "B";
+    if (position[0] == 1 && position[1] == -1) return "C";
+    if (position[0] == 0 && position[1] == -2) return "D";
+    return "0";
+  }
+  
+  public static int[] fancyMove(int[] position, int[] move) {
+    int[] newPosition = new int[2];
+    newPosition[0] = position[0] + move[0];
+    newPosition[1] = position[1] + move[1];
+    if(Math.abs(newPosition[0]) + Math.abs(newPosition[1]) > 2) return position;
+    return newPosition;
   }
 }
