18468616a63e20646dbee5f9acea011e8bf97d0c
Samina Khan <skhan191@gmail.com>
2017-01-01 03:25:10 -0500

    Day 6 part 1

---
+++ day6/Day6.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Day6 {
+  public static void main(String[] args) {
+    String[] data = readData("data.txt");
+    for(String s : data) {
+      HashMap map = charList(s);
+      char letter = maxChar(map);
+      System.out.print(letter);
+    }
+    System.out.println();
+  }
+  
+  public static char maxChar(HashMap map) {
+    int maxVal = 0;
+    char maxKey = ' ';
+    for(Object key : map.keySet()) {
+      if((int)map.get(key) > maxVal) {
+        maxKey = (char)key;
+        maxVal = (int)map.get(key);
+      }
+    }
+    return maxKey;
+  }
+  
+  public static int countChar(char c, String s) {
+    int count = 0;
+    for(int i = 0; i < s.length(); i++) {
+      if(c == s.charAt(i)) count++;
+    }
+    return count;
+  }
+  
+  public static HashMap charList(String s) {
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    for(int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if(!map.containsKey(c) && (int)c >= 97 && (int)c <= 122) map.put(c, countChar(c, s));
+    }
+    return map;
+  }
+  
+  public static String[] readData(String file) {
+    try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+      String[] cols = new String[8];
+      for(int i = 0; i < cols.length; i++) {
+        cols[i] = "";
+      }
+      String line;
+      while ((line = br.readLine()) != null) {
+        for(int i = 0; i < cols.length; i++) {
+          cols[i] = cols[i] + line.charAt(i);
+        }
+      }
+      return cols;
+    } catch(IOException e) {
+      System.out.println("file " + file + " not found");
+      return null;
+    }
+  }
+}
